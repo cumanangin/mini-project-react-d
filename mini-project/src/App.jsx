75af4b394a64c125f1c83b45363e52eaa3008e8d
riioaditya <rioadityano@gmail.com>
2022-12-25 12:57:04 +0700

    refactor : change semantic

--- mini-project/src/App.jsx
+++ mini-project/src/App.jsx
@@ -7,6 +7,7 @@ import { ApolloProvider } from '@apollo/client';
 import client from './apollo-client';
 
 import DetailMembers from './components/DetailMembers';
+
 import {
   BrowserRouter,
   Routes,
@@ -20,8 +21,7 @@ function App() {
     <ApolloProvider client = {client}>
     <BrowserRouter>
           <Routes>
-              <Route path="/" element={<Home />}
-              />
+              <Route path="/" element={<Home />}/>
               <Route path="/news" element={<News />} />
               <Route path="/members" element={<Members />} />
               <Route path="/members/:id" element={<DetailMembers />} />
--- mini-project/src/apollo-client.js
+++ mini-project/src/apollo-client.js
@@ -1,11 +1,12 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "https://mini-project-001.hasura.app/v1/graphql",
-    cache: new InMemoryCache(),
-    headers: {
-      'x-hasura-admin-secret' : 'AnkgFsb4BfSWCLh1S9s3EJfFVBvaeeAuiFFIvMlKouwMRmGBW1hhyuA0f1X76JjP',
-    },
-  });
-  
-  export default client;
+  uri: "https://mini-project-001.hasura.app/v1/graphql",
+  cache: new InMemoryCache(),
+  headers: {
+    "x-hasura-admin-secret":
+      "AnkgFsb4BfSWCLh1S9s3EJfFVBvaeeAuiFFIvMlKouwMRmGBW1hhyuA0f1X76JjP",
+  },
+});
+
+export default client;
